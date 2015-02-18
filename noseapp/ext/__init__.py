4086df69d6a6227afe3cefafe308db4b5c9dea68
Mikhail Trifonov <mikhail.trifonov@corp.mail.ru>
2015-02-18 17:18:08 +0300

    proxy import for extensions

--- noseapp/ext/__init__.py
+++ noseapp/ext/__init__.py
@@ -1,1 +1,15 @@
 # -*- coding: utf-8 -*-
+
+"""
+Реализация полностью слизана с библиотеки flask.
+"""
+
+
+def setup():
+    from ..exthook import ExtensionImporter
+    importer = ExtensionImporter(['noseapp_%s', 'noseapp.%s'], __name__)
+    importer.install()
+
+
+setup()
+del setup

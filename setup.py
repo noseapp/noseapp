5ce86686cec04ee8123e64f659a519601af6ecb1
Mikhail Trifonov <mikhail.trifonov@corp.mail.ru>
2015-02-19 22:25:13 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -6,15 +6,21 @@ from setuptools import find_packages
 import noseapp
 
 
+with open('requirements.txt') as fp:
+    requirements = [req.strip() for req in fp.readlines() if not req.startswith('--')]
+
+
 if __name__ == '__main__':
     setup(
         name='noseapp',
         version=noseapp.__version__,
+        url='https://github.com/trifonovmixail/noseapp',
         packages=find_packages(),
         author='Mikhail Trifonov',
         author_email='mikhail.trifonov@corp.mail.ru',
         description='NoseApp pattern for test development',
-        install_requires=[
-            'nose==1.3.4',
-        ],
+        include_package_data=True,
+        zip_safe=False,
+        platforms='any',
+        install_requires=requirements,
     )

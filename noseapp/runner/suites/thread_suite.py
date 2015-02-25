8fb4ff3a2d5b6d009100825c023cc2336e9a30f3
Mikhail Trifonov <mikhail.trifonov@corp.mail.ru>
2015-02-25 18:07:15 +0300

    thread suite: imap -> apply_async

--- noseapp/runner/suites/thread_suite.py
+++ noseapp/runner/suites/thread_suite.py
@@ -6,11 +6,6 @@ from multiprocessing.pool import ThreadPool
 from noseapp.runner.suites.base import BaseSuite
 
 
-def test_wrapper(data):
-    test, orig = data
-    test(orig)
-
-
 class ThreadSuite(BaseSuite):
     """
     Run tests with multiprocessing.pool.ThreadPool
@@ -23,6 +18,9 @@ class ThreadSuite(BaseSuite):
             size = cpu_count() / 2
 
         pool = ThreadPool(int(round(size)) or 2)
-        pool.imap(test_wrapper, ((t, orig) for t in self._tests))
+
+        for test in self._tests:
+            pool.apply_async(test, args=(result,))
+
         pool.close()
         pool.join()

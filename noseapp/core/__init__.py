8f31ab8b837908dd0255994de0f633d2dcaf8fce
Mikhail Trifonov <mikhail.trifonov@corp.mail.ru>
2015-04-09 17:09:41 +0300

    Extansion lazy installer

--- noseapp/core/__init__.py
+++ noseapp/core/__init__.py
@@ -5,8 +5,26 @@ from noseapp.core.collector import CollectSuite
 from noseapp.core.loader import load_suites_from_path
 
 
+class ExtensionInstaller(object):
+    """
+    Container for extension.
+    Lazy creating instance.
+    """
+
+    def __init__(self, cls, args, kwargs):
+        self.name = cls.name
+
+        self.cls = cls
+        self.args = args
+        self.kwargs = kwargs
+
+    def __call__(self):
+        return self.cls(*self.args, **self.kwargs)
+
+
 __all__ = (
     TestProgram,
     CollectSuite,
+    ExtensionInstaller,
     load_suites_from_path,
 )

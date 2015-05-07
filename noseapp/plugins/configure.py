5607f2d239c1d91fcc46e0e6122489da28b03c56
Mikhail Trifonov <mikhail.trifonov@corp.mail.ru>
2015-05-07 17:10:52 +0300

    command line options to group

--- noseapp/plugins/configure.py
+++ noseapp/plugins/configure.py
@@ -1,5 +1,6 @@
 
 import os
+from optparse import OptionGroup
 
 from noseapp.plugins.base import AppPlugin
 
@@ -10,29 +11,30 @@ class AppConfigurePlugin(AppPlugin):
 
     def options(self, parser, env=os.environ):
         super(AppConfigurePlugin, self).options(parser, env=env)
+        group = OptionGroup(parser, 'NoseApp base options')
 
-        parser.add_option(
+        group.add_option(
             '--app-processes',
             dest='app_processes',
             default=0,
             type=int,
             help='Num of processes for running. one process == one suite.'
         )
-        parser.add_option(
+        group.add_option(
             '--gevent',
             dest='gevent_pool',
             default=0,
             type=int,
             help='Num of greenlets for running. One greenlet == one suite.'
         )
-        parser.add_option(
+        group.add_option(
             '--greenlets',
             dest='gevent_greenlets',
             default=0,
             type=int,
             help='Num of greenlets for running. One greenlet == one test case.'
         )
-        parser.add_option(
+        group.add_option(
             '--threads',
             dest='thread_pool',
             default=0,
@@ -40,27 +42,29 @@ class AppConfigurePlugin(AppPlugin):
             help='Num of threads for running. If app processes is set, one thread == one test case, '
                  'else one thread == one suite.'
         )
-        parser.add_option(
+        group.add_option(
             '--ls',
             dest='ls',
             action='store_true',
             default=False,
             help='Show suites tree.'
         )
-        parser.add_option(
+        group.add_option(
             '--doc',
             dest='doc',
             action='store_true',
             default=False,
             help='Show docstring of test cases.'
         )
-        parser.add_option(
+        group.add_option(
             '-t', '--test',
             dest='run_test',
             default='',
             type=str,
             help='Test case or suite name for running.'
         )
+
+        parser.add_option_group(group)
 
         try:  # if app instance does not pushed to plugin,
             # AttributeError will be passed.
